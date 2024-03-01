a3dfa507798b5fcbc3ca2b02d27a5f3828c46806
jonathan-oberlander <jonathan.oberlander@gmail.com>
2024-03-01 12:05:43 +0000

    vite - react - typescript - panda - eslint - prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'styled-system'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'unused-imports'],
+  settings: {
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { css } from '@styled-system/css'
+
+function App() {
+  return (
+    <div className={css({ fontSize: '2xl', fontWeight: 'extralight' })}>
+      Hello 🐼!
+    </div>
+  )
+}
+
+export default App
